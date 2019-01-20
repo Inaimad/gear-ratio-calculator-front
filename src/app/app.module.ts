cf1b2c8588357ac31d0ca96042900783e483ea18
Damian Igielski <damian.igielski@outlook.com>
2019-01-20 15:35:03 +0100

    Created a proper navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { ManualComponent } from './manual/manual.component';
 import { AutomaticComponent } from './automatic/automatic.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { ResultComponent } from './result/result.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ManualComponent,
     AutomaticComponent,
-    ResultComponent
+    ResultComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
